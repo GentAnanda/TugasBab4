8bcb0c2540063951f5bf10c85e3b7d2f5698825a
GentAnanda <genta.ananda@gmail.com>
2025-05-29 20:53:44 +0700

    first commit

---
+++ App.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import {Bookmark, Discover, Favorite, Home, Profile} from './src/screens';
+import Setting from './src/screens/Setting';
+export default function App() {
+  return <Favorite />;
+}
---
+++ src/screens/index.jsx
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Discover from "./Discover";
+import Bookmark from "./Bookmark";
+import Profile from "./Profile";
+import Setting from "./Setting";
+import Favorite from "./Favorite";
+export {Home, Discover, Bookmark, Profile, Setting, Favorite}
